53ef72e7609c94a2e933fceb0529496ee19549d7
Frank Li <b439988l@gmail.com>
2019-06-21 16:25:32 +0800

    express gen stuff

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
-//Modules
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'build')));
+
+require('dotenv').config();
 
 //db connection
 mongoose.connect(process.env.DB_CON)
@@ -13,19 +23,25 @@ mongoose.connect(process.env.DB_CON)
 const postsRouter = require('./posts');
 const resultsRouter = require('./result')
 
-//Express Server
-const app = express();
-
-app.use(express.json())
-
-app.use(express.static('./build/'))
-
 //Catch routes
 app.use('/api', postsRouter);
 app.use('/api', resultsRouter)
 
 
-const port = process.env.PORT || 3000
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.listen(port, () => console.log(`ThreePointSix API listening on port ${port}`))
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 module.exports = app;
