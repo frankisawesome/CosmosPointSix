013c462f6fd768c6f5cfd582fd4c7e9cb8465d30
Frank Li <b439988l@gmail.com>
2019-06-22 10:40:08 +0800

    added static files

---
+++ build/precache-manifest.650e493127c16a4a99a5796e6644f09a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a334fd9eea819f094f85411370c7b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a5a18104cd36277180",
+    "url": "/static/css/main.87100868.chunk.css"
+  },
+  {
+    "revision": "2736200ca70bb81728ef",
+    "url": "/static/js/2.05078cde.chunk.js"
+  },
+  {
+    "revision": "93a5a18104cd36277180",
+    "url": "/static/js/main.e58238c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
